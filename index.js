0ba374c8bfe1ec4dee06f98557b8e2100f8161ef
Aisha Hussein <aishasalaam07@gmail.com>
2023-03-19 12:34:50 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob"  
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+
+}
+
+
+function setBestCustomer() {
+  return  bestCustomer = "not bob";
+
+}
+// overwriteBestCustomer()
+function overwriteBestCustomer(){
+  return bestCustomer =  'maybe bob';
+}
+
+
+function changeLeastFavoriteCustomer(){
+  throw 'Assignment to constant variable.';
+}
+
